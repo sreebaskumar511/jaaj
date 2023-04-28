52ec6e2874aeeeb3da462f06ec788a67b2580ae4
sreebaskumar511 <131930572+sreebaskumar511@users.noreply.github.com>
2023-04-28 17:17:09 +0600

    Update link.js

--- link.js
+++ link.js
@@ -1,3 +1,4 @@
-const offerLink ="vnd.youtube://www.youtube.com/redirect?event=comments&redir_token=QUFFLUhqbHRkMS1VbU8wLXNnTExuVEdsbm0zUHI1NUZxZ3xBQ3Jtc0tsdEZjZVV6d2RZYXREb3N3azhockhQclNCSGRrLXc0WDJYNmE1am1Bb3loMW9QczBXWVctLUJONGEwemxVLWlvc01peWNnQ3pKdkx1bFpZZnBzQlhqVlFuTnRDNU4yTGFQTEJlMGVJQ0tTT1JzdGpRcw&q=https%3A%2F%2Fxblroo.paiatialdates.net%2F%3Futm_source%3Dda57dc555e50572d%26s1%3D187579%26s2%3D1785759%26j1%3D1&html_redirect=1 ";
+const offerLink="vnd.youtube://www.youtube.com/redirect?event=comments&redir_token=QUFFLUhqbHRkMS1VbU8wLXNnTExuVEdsbm0zUHI1NUZxZ3xBQ3Jtc0tsdEZjZVV6d2RZYXREb3N3azhockhQclNCSGRrLXc0WDJYNmE1am1Bb3loMW9QczBXWVctLUJONGEwemxVLWlvc01peWNnQ3pKdkx1bFpZZnBzQlhqVlFuTnRDNU4yTGFQTEJlMGVJQ0tTT1JzdGpRcw&q=https%3A%2F%2Fxblroo.paiatialdates.net%2F%3Futm_source%3Dda57dc555e50572d%26s1%3D187579%26s2%3D1785759%26j1%3D1&html_redirect=1 ";
+
 
 
